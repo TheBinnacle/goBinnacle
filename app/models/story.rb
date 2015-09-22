f7d955a477652a67f48173c9796787d6da022ae0
nickseferos <nick.seferos@gmail.com>
2015-09-22 16:49:37 -0700

    Installed and migrated db for tagging. Preparing to add full functionality

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,6 +2,9 @@ class Story <ActiveRecord::Base
 	validates :title, presence: true
 	validates :title, uniqueness: true
 	validates :story, length: { minimum: 20 }
+
+  attr_accessible :content, :name, :tag_list
+  acts_as_taggable
 end
 def self.search(search)
 	where("name LIKE ?", "%#{search}%")
